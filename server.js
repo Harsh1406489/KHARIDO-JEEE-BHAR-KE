92020fcc6920fe31bd8d1bea6e72a0b9ac6530cc
Harsh Sultania <harshsultania@Harshs-MacBook-Air.local>
2025-01-12 12:22:38 +0530

    categories api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 app.get("/", (req, res) => {
     res.send("<h1>Welcome dosto</h1>");
